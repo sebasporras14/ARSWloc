77f9d4badf9365d0c6386f90bbfafa3682c1fb63
sebasporras14 <sebastian.porras@mail.escuelaing.edu.co>
2023-06-14 18:53:55 -0500

    cambios

--- src/main/java/edu/escuelaing/arsw/app/CountLines.java
+++ src/main/java/edu/escuelaing/arsw/app/CountLines.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class CountLines {
 
     
-    /** 
+    /* 
      * execute the program according the parameters given 
      * @param args
      * @throws IOException
@@ -14,7 +14,6 @@ public class CountLines {
         String type = args[0];
         String path = args[1];
         String pattern = "*.java";
-        // use the class counter and method CountLines to start counting according the type
         Counter.CountLines(type, path, pattern);
         System.out.println("phy o loc: " + type + " path: " + path + " pattern: " + pattern );
     }
